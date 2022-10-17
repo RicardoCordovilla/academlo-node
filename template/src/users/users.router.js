ec33b2cb922e6113a953de35c9f7d691a24c4bc8
RicardoCordovilla <ricardocordovillaf@gmail.com>
2022-10-17 15:41:02 -0500

    template added

---
+++ template/src/users/users.router.js
@@ -0,0 +1,48 @@
+const router = require('express').Router()
+const passport = require('passport')
+const userServices = require('./users.services')
+
+
+require('../middlewares/auth.middleware')(passport)
+
+// rutas raiz
+router.get('/',
+    passport.authenticate('jwt', { session: false }),
+    userServices.getAllUsers)
+
+
+// ruta de informacion propia del usuario loggeado
+// el orden de las rutas es importante para gestionar los parametros
+router.route('/me')
+    .get(
+        passport.authenticate('jwt', { session: false }),
+        userServices.getMyUser
+    )
+    .patch(
+        passport.authenticate('jwt', { session: false }),
+        userServices.patchMyUser
+    )
+    .delete(
+        passport.authenticate('jwt', { session: false }),
+        userServices.deleteMyUser
+    )
+
+
+
+// rutas dinamicas por ID
+// router.get('/:id)
+// router.patch('/:id)
+// router.put('/:id)
+// router.delete('/:id)
+
+// /api/v1/users/:id
+router.route('/:id')
+    .get(userServices.getUserById)
+    .patch(userServices.patchUser)
+    .delete(userServices.deleteUser)
+
+
+
+
+
+module.exports = router
